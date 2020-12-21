76c31187497e04711e93a5359ba9b7bd8c964f04
YuseiMizuno <mizuno.yusei.2207@members.co.jp>
2020-12-21 20:33:56 +0900

    microCMSからAPIを引っ張ってきてトップページに表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,46 @@
 import Link from 'next/link'
-import Layout from '../components/Layout'
+import React from 'react';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const Home = ({blogs}: any) => {
+  return (
+    <div>
+      <h2>最新の記事</h2>
+      <div>
+        {blogs.map((blog: any) => (
+          <React.Fragment key={blog.id}>
+            <Link href="/blogs/[id]" as={`blogs/${blog.id}`}>
+              <a>
+                <h2>{blog.title}</h2>
+              </a>
+            </Link>
+            {blog.tags.map((tag: any) => (
+              <React.Fragment key={tag.id}>
+                <span>{tag.name}</span>
+              </React.Fragment>
+            ))}
+          </React.Fragment>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default IndexPage
+export const getStaticProps = async () => {
+  const key = {
+    headers: {'X-API-KEY': process.env.API_KEY || ''},
+  };
+
+  const res = await fetch(
+    `https://mizmiz-hls-blog.microcms.io/api/v1/blogs/`,
+    key,
+  );
+  const data = await res.json();
+
+  return {
+    props: {
+      blogs: data.contents,
+    }
+  }
+};
+
+export default Home
